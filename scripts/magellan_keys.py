abe3e85e9aad825e1e99f7de9cb744dcc5095fad
Scott Devoid <devoid@anl.gov>
2014-07-01 12:52:15 -0500

    more updates to userbase scripts

--- scripts/magellan_keys.py
+++ scripts/magellan_keys.py
@@ -16,29 +16,6 @@ for name in logger_names:
     _logger = logging.getLogger(name)
     _logger.addHandler(logging.NullHandler())
 
-parser = argparse.ArgumentParser(
-    description="""
-Add or remove a key for a user
-"""
-)
-parser.add_argument('-u', dest='user',
-    help="The username for the user, not the user_id.",
-    type=str, required=True)
-parser.add_argument('-p', dest='password', type=str,
-    help="Set the user's password.", required=True)
-parser.add_argument('-n', dest='name', type=str,
-    help="Name to use for the key", required=True)
-parser.add_argument('-r', dest='rename', type=str,
-    help="If this is set, rename key matching 'name' to 'rename' value.")
-parser.add_argument('--delete', action='store_const', 
-        const=True, help="Delete the key")
-parser.add_argument('--insecure', action='store_const', const=True,
-    help="Do not validate SSL Certificate when connecting to Keystone"
-)
-parser.add_argument('key_parts', nargs=argparse.REMAINDER)
-
-args = parser.parse_args()
-key_string = " ".join(args.key_parts) + '\n'
 
 def fix_key_name(name):
     rx = re.compile('\W')
@@ -47,9 +24,10 @@ def fix_key_name(name):
     fixed_name = rx.sub(replace, name)
     return fixed_name if len(fixed_name) else 'default' 
 
-def ensure_key (nova, args, key_string):
+
+def ensure_key(nova, args, key_string):
     args.name = fix_key_name(args.name)
-    matches = [ key for key in nova.keypairs.list() if key.name == args.name ]
+    matches = [key for key in nova.keypairs.list() if key.name == args.name]
     key_exists = True if len(matches) else False
     if args.delete and key_exists:
         nova.keypairs.delete(args.name)
@@ -68,28 +46,49 @@ def ensure_key (nova, args, key_string):
     else:
         nova.keypairs.create(args.name, public_key=key_string)
 
-def get_nova(auth_url, region, args):
-    # Nova API requires a tenant so first get that
-    # via keystone
-    keystone = keystone_client.Client(
-        username=args.user,
-        password=args.password,
-        auth_url=auth_url)
+
+def get_nova(cfg, args):
+    k_cfg = ['username', 'password', 'auth_url']
+    keystone_kwargs = dict([(k,v) for (k,v) in cfg.iteritems() if k in k_cfg])
+    keystone = keystone_client.Client(**keystone_kwargs)
     tenants = keystone.tenants.list()
-    if len(tenants) > 0:
-        return nova_client.Client(args.user, args.password,
-                tenants[0].name, auth_url, region_name=region, endpoint_type='internalURL', service_type='compute')
-    else:
-        sys.exit(0)
+    if len(tenants) == 0:
+        sys.exit(-1)
+    return nova_client.Client(args.user, args.password, tenants[0].name,
+                              cfg['auth_url'], endpoint_type='internalURL',
+                              service_type='compute')
+    
 
-cfg_file = os.path.expanduser("~/userbase.config")
-if not os.path.exists(cfg_file):
-    print "Config file " + cfg_file + " does not exist!"
-    sys.exit(1)
-cfg = ConfigParser.ConfigParser()
-cfg.read(cfg_file)
+def main():
+    cfg_file = os.path.expanduser("~/userbase.config")
+    if not os.path.exists(cfg_file):
+        print "Config file " + cfg_file + " does not exist!"
+        sys.exit(1)
+    parser = argparse.ArgumentParser(
+        description="Add or remove a key for a user")
+    parser.add_argument('-u', dest='user',
+        help="The username for the user, not the user_id.",
+        type=str, required=True)
+    parser.add_argument('-p', dest='password', type=str,
+        help="Set the user's password.", required=True)
+    parser.add_argument('-n', dest='name', type=str,
+        help="Name to use for the key", required=True)
+    parser.add_argument('-r', dest='rename', type=str,
+        help="If this is set, rename key matching 'name' to 'rename' value.")
+    parser.add_argument('--delete', action='store_const', 
+        const=True, help="Delete the key")
+    parser.add_argument('--insecure', action='store_const', const=True,
+        help="Do not validate SSL Certificate when connecting to Keystone")
+    parser.add_argument('key_parts', nargs=argparse.REMAINDER)
+    args = parser.parse_args()
+    key_string = " ".join(args.key_parts) + '\n'
+    cfg = ConfigParser.ConfigParser()
+    cfg.read(cfg_file)
+    for section in cfg.sections():
+        nova = get_nova(dict(cfg.items(section)), args)
+        if nova is None:
+            continue
+        ensure_key(nova, args, key_string)
 
-auth_url = cfg.get("env", "auth_url")
-region   = cfg.get("env", "region_name") 
-nova = get_nova(auth_url, region, args)
-ensure_key(nova, args, key_string)
+if __name__ == '__main__':
+    main()
--- scripts/magellan_user.py
+++ scripts/magellan_user.py
@@ -2,68 +2,87 @@
 import argparse
 import ConfigParser
 import os
+import subprocess
 import sys
 from keystoneclient.exceptions import NotFound
 from keystoneclient.v2_0 import client
 
-parser = argparse.ArgumentParser(
-    description="""
-Update a user in Magellan.
+def main():
+    desc = """Update a user in Magellan.
 
 If the tenant does not exist this will return an error. If the user
 does not exist, he or she will be added to the system iff a password
 is supplied.
 """
-)
-parser.add_argument('-u', dest='user',
-    help="The username for the user, not the user_id.",
-    type=str, required=True)
-parser.add_argument('-t', dest='tenant',
-    help="The tenant name, not the tenant_id.", type=str)
-parser.add_argument('--is-admin', action='store_const', dest='is_admin', const=True,
-    help="Ensure that the user has 'admin' privileges on the tenant.")
-parser.add_argument('--disable', action='store_const', const=True,
-    help="Disable the user.")
-parser.add_argument('--evict', action='store_const', const=True,
-    help="Remove the user from that tenant, removing both admin and non-admin privileges")
-parser.add_argument('--delete', action='store_const', const=True,
-    help="Delete the user completely")
-parser.add_argument('-p', dest='password', type=str,
-    help="Set the user's password.")
-parser.add_argument('-e', dest='email', type=str,
-    help="Set the user's email.")
-parser.add_argument('--insecure', action='store_const', const=True,
-    help="Do not validate SSL Certificate when connecting to Keystone"
-)
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('-u', dest='user',
+                        help="The username for the user, not the user_id.",
+                        type=str, required=True)
+    parser.add_argument('-t', dest='tenant', type=str,
+                        help="The tenant name, not the tenant_id.")
+    parser.add_argument('--is-admin', action='store_const',
+                        dest='is_admin', const=True, help="Ensure that "
+                        "the user has 'admin' privileges on the tenant.")
+    parser.add_argument('--disable', action='store_const', const=True,
+                        help="Disable the user.")
+    parser.add_argument('--evict', action='store_const', const=True,
+                        help="Remove the user from that tenant, removing "
+                        "both admin and non-admin privileges")
+    parser.add_argument('--delete', action='store_const', const=True,
+                        help="Delete the user completely")
+    parser.add_argument('-p', dest='password', type=str,
+                        help="Set the user's password.")
+    parser.add_argument('-e', dest='email', type=str,
+                        help="Set the user's email.")
+    parser.add_argument('--insecure', action='store_const', const=True,
+                        help="Do not validate SSL Certificate when "
+                        "connecting to Keystone")
+    args = parser.parse_args()
 
-args = parser.parse_args()
+    # We pull all environment variables from a config file.
+    # Each section designates a separate OpenStack env that we must
+    # independently update.
+    cfg_file = os.path.expanduser("~/userbase.config")
+    if not os.path.exists(cfg_file):
+        print "Config file %s does not exist!" % cfg_file
+        sys.exit(1)
+    cfg = ConfigParser.ConfigParser()
+    cfg.read(cfg_file)
+    for section in cfg.sections():
+        apply_settings_for_os_env(dict(cfg.items(section)), args)
 
-cfg_file = os.path.expanduser("~/userbase.config")
-if not os.path.exists(cfg_file):
-    print "Config file " + cfg_file + " does not exist!"
-    sys.exit(1)
-cfg = ConfigParser.ConfigParser()
-cfg.read(cfg_file)
-
-keystone = client.Client(
-    username =    cfg.get("env", "username"),
-    password =    cfg.get("env", "password"),
-    tenant_name = cfg.get("env", "tenant_name"),
-    auth_url =    cfg.get("env", "auth_url"),
-)
-# Get standard admin and member roles:
-member_role = [ r for r in keystone.roles.list() if r.name == "Member"][0]
-admin_role  = [ r for r in keystone.roles.list() if r.name == "admin"][0] 
+def apply_settings_for_os_env(cfg, args):
+    for k,v in cfg.iteritems():
+        os.environ[k] = v
+    keystone = client.Client(
+        username =    cfg.get("username"),
+        password =    cfg.get("password"),
+        tenant_name = cfg.get("tenant_name"),
+        auth_url =    cfg.get("auth_url"))
+    # Get standard admin and member roles:
+    admin_role_name = os.environ.get('admin_role', 'admin')
+    member_role = [r for r in keystone.roles.list() if r.name == "Member"][0]
+    admin_role  = [r for r in keystone.roles.list() if
+                   r.name == admin_role_name][0] 
+    if args.delete:
+        delete_user(keystone, args)
+    else:
+        ensure_tenant(keystone, args)
+        ensure_user(keystone, args)
+        if args.tenant != None:
+            update_user_tenant(keystone, args, admin_role=admin_role, member_role=member_role)
 
 def get_user (name, keystone):
-    matched = [ user for user in keystone.users.list() if user.name == args.user]
+    matched = [u for u in keystone.users.list() if u.name == name]
     return matched[0] if len(matched) else None
 
-def delete_user (keystone, args):
+def delete_user(keystone, args):
     user = get_user(args.user, keystone) 
     user.delete()
 
-def ensure_tenant (keystone, args):
+def ensure_tenant(keystone, args):
+    if not args.tenant:
+        return
     matched = [ tenant for tenant in keystone.tenants.list()
         if tenant.name == args.tenant ]
     if len(matched):
@@ -71,7 +90,7 @@ def ensure_tenant (keystone, args):
     else:
         return keystone.tenants.create(args.tenant)
 
-def ensure_user (keystone, args):
+def ensure_user(keystone, args):
     user = get_user(args.user, keystone)
     # If no user was found, create the user with password and email
     if user is None:
@@ -91,7 +110,7 @@ def ensure_user (keystone, args):
         if args.email:
             keystone.users.update(user, email=args.email)
 
-def update_user_tenant (keystone, args):
+def update_user_tenant(keystone, args, admin_role=None, member_role=None):
     user = get_user(args.user, keystone)
     tenant = [ t for t in keystone.tenants.list() if t.name == args.tenant ][0]
     if tenant is None:
@@ -99,11 +118,11 @@ def update_user_tenant (keystone, args):
         print "Tenant not found!"
         sys.exit(1)
     # Remove the admin role for the user if we did not set the admin flag
-    if not args.is_admin:
-        try:
-            keystone.roles.remove_user_role(user, admin_role, tenant=tenant)
-        except NotFound:
-            pass
+    #if not args.is_admin:
+    #    try:
+    #        keystone.roles.remove_user_role(user, admin_role, tenant=tenant)
+    #    except Exception:
+    #        pass
     if args.evict:
         # Evicting the user from the tenant
         try:
@@ -117,15 +136,18 @@ def update_user_tenant (keystone, args):
     else:
         # Adding the user to the tenant with the correct role
         role = admin_role if args.is_admin else member_role
+        ub_user = os.environ.get('username')
+        ub_pass = os.environ.get('password')
+        ub_tenant = os.environ.get('tenant_name')
+        auth_url = os.environ.get('auth_url')
         try:
-            keystone.roles.add_user_role(user, role, tenant=tenant)
-        except NotFound:
-            pass
-        
-if args.delete:
-    delete_user(keystone, args)
-else:
-    ensure_tenant(keystone, args)
-    ensure_user(keystone, args)
-    if args.tenant != None:
-        update_user_tenant(keystone, args)
+            subprocess.check_output(["keystone", "--os_username", ub_user, "--os_password", ub_pass,
+                                     "--os_tenant_name", ub_tenant, "--os_auth_url", auth_url,
+                                     "user-role-add", "--user", user.id, "--tenant_id", tenant.id,
+                                     "--role", role.id])
+        except Exception, e:
+            print "Error adding %s user to %s tenant: %s" % (user.name, tenant.name, e)
+            sys.exit(1)
+
+if __name__ == '__main__':
+    main()
